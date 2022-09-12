e65a94455ced295b9f6316bd7392957e4de44adf
Muhammad Ahmed <sss.ahm7@gmail.com>
2022-09-12 17:35:06 +0500

    our team section banner created

--- our-team.php
+++ our-team.php
@@ -2,12 +2,36 @@
 @include('header.php')
 ?>
 <section>
-    <div class="container">
-
+    <div class="inner-page-banner overflow-hidden position-relative m-auto">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-sm-12 col-md-6 col-lg-6 order-xs-2">
+                    <div class="team-banner">
+                        <h2>Our Team</h2>
+                        <p>
+                            We have been the most trusted “Health care Nutrition” manual for 25 years, We are proud of
+                            our rich history of providing evidence based nutrition resources.
+                        </p>
+                        <div class="d-flex justify-content-center home-wrapper">
+                            <button class="learn-more">
+                                <span class="circle" aria-hidden="true">
+                                    <span class="icon arrow"></span>
+                                </span>
+                                <span class="button-text">Get Started</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-6 col-lg-6 order-xs-1">
+                    <div class="d-flex justify-content-center">
+                        <img width="50%" class="img-fluid" src="images/our-team-banner.svg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
-
 <?php
-@include('footer.php')
+    @include('footer.php')
 ?>
