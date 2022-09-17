f1cd7666250f47830e3e1041e15d1b9c852aca11
Muhammad Ahmed <sss.ahm7@gmail.com>
2022-09-18 02:07:22 +0500

    contact page updated

--- contact.php
+++ contact.php
@@ -34,9 +34,68 @@
         </div>
     </div>
 </section>
+
+<section class="contact-cards pt-5 pb-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-4 py-2">
+                <div data-aos="fade-up" data-aos-duration="1000" class="card card-1">
+                    <h4>Main Branch</h4>
+                    <p>
+                        DE 198 Tech Road
+                        <br>
+                        Bridge Str, Newyork 10026
+                        <br>
+                        United States.
+                    </p>
+                    <a href="#">Get Directions</a>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-4 py-2">
+                <div data-aos="fade-up" data-aos-duration="1000" class="card card-2">
+                    <h4>Quick Connect</h4>
+                    <!-- phone icon is spossed to be here.  -->
+                    <p>
+                        +92-300-0000000
+                        <br>
+                        +92-300-0000000
+                    </p>
+                    <!-- email icon is spossed to be here  -->
+                    <p>
+                        info@nutrional-pakistan.com
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-4 py-2">
+                <div data-aos="fade-up" data-aos-duration="1000" class="card card-3">
+                    <h4>Career Inquiries</h4>
+                    <h5>John Martin</h5>
+                    <p class="mb-2">
+                        Ph: (+01) 123 456 7899
+                        <br>
+                        Ph: (+01) 123 456 7899
+                    </p>
+                    <p class="mb-0">
+                        Email: info@example.com
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
 <section>
     <div class="container">
-
+        <div class="my-5">
+            <iframe class="map"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d435518.681783653!2d74.05418981777392!3d31.48322087499928!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39190483e58107d9%3A0xc23abe6ccc7e2462!2sLahore%2C%20Punjab%2C%20Pakistan!5e0!3m2!1sen!2s!4v1663444724754!5m2!1sen!2s"
+                width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
     </div>
 </section>
 
